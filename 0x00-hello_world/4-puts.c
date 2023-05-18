93c7ea80fca096746002fbdcfb72015ab897a695
bowashon <baidoofelix05@gmail.com>
2023-05-18 09:58:20 +0000

    print to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - function to print string to stdout
+ * puts()- funtion to print string to stdout
+ * Return: return o
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
