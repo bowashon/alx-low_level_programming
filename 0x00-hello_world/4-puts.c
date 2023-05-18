a3af17682c1901199a5dc70a06a2dd4cf5e04f63
bowashon <baidoofelix05@gmail.com>
2023-05-18 10:19:16 +0000

    print to stdout using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * puts()- funtion to print string to stdout
  * Return: return o
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
