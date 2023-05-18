20535edd7ac175e46e61881e2104344abbfbe4e0
bowashon <baidoofelix05@gmail.com>
2023-05-18 11:26:21 +0000

    print size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - function to input the size of various types
+ *
+ * Return: return 0 success
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
+}
