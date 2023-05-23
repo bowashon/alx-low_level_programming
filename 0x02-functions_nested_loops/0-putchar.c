1ea75b858148a499a8c47520aaba492ca02af165
bowashon <baidoofelix05@gmail.com>
2023-05-23 13:30:45 +0000

    program that print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,6 +9,7 @@
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -18,5 +19,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
