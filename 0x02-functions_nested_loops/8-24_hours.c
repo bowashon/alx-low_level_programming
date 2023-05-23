e113c6b7fa5d5df5a4512820a67bfea01acea6d2
bowashon <baidoofelix05@gmail.com>
2023-05-23 19:23:32 +0000

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@
  */
 void jack_bauer(void)
 {
-	int i,j,k,l;
+	int i, j, k, l;
 
 	for (i = 0; i <= 2; i++)
 	{
@@ -18,7 +18,7 @@ void jack_bauer(void)
 	{
 	for (l = 0; l <= 9; l++)
 	{
-	if (i == 2 && j == 4 && k == 0 && l == 0)
+		if (i == 2 && j == 4 && k == 0 && l == 0)
 	{
 			return;
 	}
