420c96cd0fa31ebb8ecb3cdb9e28e81572607961
bowashon <baidoofelix05@gmail.com>
2023-05-23 13:27:22 +0000

    program that print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints to the stdout
+ *
+ * _putchar - write to the stdout
+ *
+ * Return: return 0 success
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE
+#define HEARER_FILE
+int _putchar(char c);
+#endif
