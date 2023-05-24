aa83a9b68f6562e16b23f7a35cb29e249256cbc7
bowashon <baidoofelix05@gmail.com>
2023-05-24 03:01:02 +0000

    print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,23 +15,25 @@ void times_table(void)
 	for (j = 0; j <= 9; j++)
 	{
 		k = i * j;
-	if (k <= 9 || j == 0 || i == 0)
+	if (k <= 9)
 	{
 	_putchar(k + '0');
+	if(j < 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	}
 	}
 	else
 	{
 	_putchar(k / 10 + '0');
 	_putchar(k % 10 + '0');
-	}
-	if (j < 9)
+	if(j < 9)
 	{
 	_putchar(',');
 	_putchar(' ');
 	}
-	if (k < 9)
-	{
-	_putchar(' ');
 	}
 	}
 	_putchar('\n');
