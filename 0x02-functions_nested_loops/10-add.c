ce473ea5466f197ddc5d04a615a2216b537f0b05
bowashon <baidoofelix05@gmail.com>
2023-05-24 03:10:55 +0000

    finds the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - finds the sum of two integers
+ * @a: parameter to be added
+ * @b: parameter to be added
+ * Return: return sum
+ */
+int add(int a, int b)
+{
+	int sum = 0;
+
+	sum = a + b;
+	return (sum);
+}
