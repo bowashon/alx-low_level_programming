0a307c0b6cdc317db2a2d63d2c17961626fa78fe
bowashon <baidoofelix05@gmail.com>
2023-05-25 10:05:50 +0000

    comment out

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	while (i < 10)
 	{
 		putchar(i);
-		/* i is not increamneted so it will always print 0*/
-		/* i is not increamented so i is always less than 10*/
-	}
+	} /* i is not increamented */
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
