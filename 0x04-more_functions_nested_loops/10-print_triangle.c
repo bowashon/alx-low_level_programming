ca59f21414905def4a6f4b3dbdd9833a07496a71
bowashon <baidoofelix05@gmail.com>
2023-05-26 14:22:12 +0000

    print right angle triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - function to print traingle
+ * @size: parameter to print
+ *
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (k = size; k >= i; k--)
+		{
+			_putchar(' ');
+		}
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
