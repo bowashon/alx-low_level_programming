9219d8aeae92ef6f3d557a70ae1cf293df05bdc8
bowashon <baidoofelix05@gmail.com>
2023-05-26 08:33:46 +0000

    print squares using #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints squares of given size
+ *
+ * @size: parameter to be printed
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		}
+	}
+}
