6564459c86092b75d680b533f0b0705ab4ad8f30
bowashon <baidoofelix05@gmail.com>
2023-05-27 10:15:57 +0000

    find largest prime factor of a number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * main - function to print largest prime factor of a given numberr
+ *
+ *
+ * Return: return 0 success
+ */
+
+int main(void)
+{
+	long long int num = 612852475143;
+	int fac;
+	int largest_prime_fac = 1;
+	double square = sqrt(num);
+
+	while (num % 2 == 0)
+	{
+		largest_prime_fac = 2;
+
+		num = num / 2;
+	}
+
+	for (fac = 3; fac <= square; fac += 2)
+	{
+		while (num % fac == 0)
+		{
+			largest_prime_fac = fac;
+
+			num = num / fac;
+		}
+	}
+
+	if (num > largest_prime_fac)
+	{
+		largest_prime_fac = num;
+	}
+
+	printf("%d", largest_prime_fac);
+
+	return (0);
+}
+
+
