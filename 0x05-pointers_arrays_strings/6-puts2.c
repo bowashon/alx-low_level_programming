8293feeaf0d419822abd32785115d090563c5a0b
bowashon <baidoofelix05@gmail.com>
2023-05-29 19:59:55 +0000

    print every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other function starting from the first character
+ *
+ * @str: parameter to print
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		_putchar(str[len]);
+
+		len += 2;
+	}
+	_putchar('\n');
+}
