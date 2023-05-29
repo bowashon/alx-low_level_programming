283dae7bff09ed5e0d85574f4a30c1a26ec5dad8
bowashon <baidoofelix05@gmail.com>
2023-05-29 21:27:12 +0000

    print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,18 @@
 void puts2(char *str)
 {
 	int len = 0;
+	int i = 0;
 
 	while (str[len] != '\0')
 	{
-		_putchar(str[len]);
-
-		len += 1;
 		len++;
+	}
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+
+		i += 2;
 	}
 	_putchar('\n');
 }
