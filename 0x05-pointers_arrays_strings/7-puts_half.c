efb565bb6b1e86727ea8ba0e53022051fbbba575
bowashon <baidoofelix05@gmail.com>
2023-05-30 14:39:33 +0000

    print other half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,16 @@
 void puts_half(char *str)
 {
 	int i;
-	int len = 0;
-	int half_len = 0;
+	long len = 0;
+	long half_len = 0;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	half_len = len % 2 == 0 ? len / 2 : (len - 1) / 2;
-	
+	half_len = len % 2 == 0 ? len / 2 : (len + 1) / 2;
+
 	for (i = half_len; i < len; i++)
 	{
 		_putchar(str[i]);
