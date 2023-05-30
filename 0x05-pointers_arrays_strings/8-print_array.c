ef7b346fca04a829eab2656c26f18a90c7acb5ea
bowashon <baidoofelix05@gmail.com>
2023-05-30 15:15:37 +0000

    print integes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+		return;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 
-		if (i != a[n - 1])
+		if (i != n - 1)
 		{
 			printf(", ");
 		}
