b1aa2574096aab786befa2f4fd390a38615f65a8
bowashon <baidoofelix05@gmail.com>
2023-05-31 20:43:11 +0000

    copy n character of a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string form source to destination
+ * @dest: destination file
+ * @src: source file
+ * @n: number of characters to copy
+ * Return: return a pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
