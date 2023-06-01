67a2c670e3e565b6e90449d806ca3c376a9e9b4c
bowashon <baidoofelix05@gmail.com>
2023-06-01 13:51:56 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,14 +16,10 @@ int _strcmp(char *s1, char *s2)
 		i++;
 	}
 
-	if (s1[i] < s2[i])
+	if (s1[i] != s2[i])
 	{
-		return (-15);
+		return (s1[i] - s2[i]);
 	}
-	else if (s1[i] > s2[i])
-	{
-		return (15);
-}
 
 	return (0);
 }
