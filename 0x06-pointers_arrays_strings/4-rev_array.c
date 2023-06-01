70d8e6a87239d3647c0dd57dc00c9ffc415673a2
bowashon <baidoofelix05@gmail.com>
2023-06-01 15:25:54 +0000

    reverse array;

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -15,8 +15,12 @@ void reverse_array(int *a, int n)
 	while (i < n)
 	{
 		temp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = temp;
+
+		a[i] = a[n - 1];
+
+		a[n - 1] = temp;
+
 		i++;
+		n--;
 	}
 }
