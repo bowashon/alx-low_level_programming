1d3f75d5481eda889c04e9400dda807cc9ad6753
bowashon <baidoofelix05@gmail.com>
2023-06-04 17:18:28 +0000

    first 50 fibonicci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that prints the first 50 fibonacci series
+ *
+ * Return: return always 0 success
+ *
+ */
+int main(void)
+{
+	int i;
+	long long first = 0;
+	long long second = 1;
+	long long result;
+
+	printf("%lld, %lld,  ", first, second);
+
+	i = 3;
+	while (i <= 50)
+	{
+		if (i > 3)
+		{
+			printf(", ");
+		}
+		result = first + second;
+		printf("%lld", result);
+		first = second;
+		second = result;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
