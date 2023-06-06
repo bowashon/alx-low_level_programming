90e307a9e7ca56692a576271556b21521821271f
bowashon <baidoofelix05@gmail.com>
2023-06-06 01:20:26 +0000

    costum strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - function to locate a character in a string
@@ -17,5 +16,5 @@ char *_strchr(char *s, char c)
 		return (&s[i]);
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
