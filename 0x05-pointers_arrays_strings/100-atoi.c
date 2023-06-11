d54a29cac1230a4f29df496f03b390612ed50fc9
bowashon <baidoofelix05@gmail.com>
2023-06-11 15:16:18 +0000

    custom atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert string repesentation of an integer
+ * tp an integer
+ * @s: pointer to the string
+ * Return: retrn integer values of string representative
+ */
+int _atoi(char *s)
+{
+	unsigned int result = 0;
+	int sign = 1;
+	int i = 0;
+	int is_digit = 0;
+
+	while (s[i])
+	{
+	if (s[i] == '-' || s[i] == '+')
+	{
+		sign = (s[i] == '-') ? -1 : 1;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		is_digit = 1;
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
+	if (is_digit == 1)
+	{
+		break;
+	}
+	i++;
+	}
+	return (result * sign);
+}
