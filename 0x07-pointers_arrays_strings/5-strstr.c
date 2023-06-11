e7b89c80370d9481ee3bc2befa252e8b76aa401a
bowashon <baidoofelix05@gmail.com>
2023-06-11 20:22:11 +0000

    custom function of the strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * _strstr -  function that locates a string
+ * @haystack: pointer to the main string
+ * @needle: pointer to the substring
+ * Return: return a pointer to the beginning of the located substring, or
+ * NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	bool match = true;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+
+	while (*haystack != '\0')
+	{
+		char *n = needle;
+		char *h = haystack;
+		match = true;
+		while (*n != '\0')
+		{
+			if (*h != *n)
+			{
+				match = false;
+				break;
+			}
+
+		n++;
+		h++;
+	}
+
+	if (match)
+	{
+		return (haystack);
+	}
+
+	haystack++;
+
+	}
+	return (NULL);
+}
+
+
