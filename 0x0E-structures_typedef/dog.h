fa82b4064d8c5a6f4b5ff2a4af2ece44d0a920f2
bowashon <baidoofelix05@gmail.com>
2023-06-20 11:34:42 +0000

    create new name for the struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 /**
  * struct dog - defines new type with elements
- * dog_t: new name of the strct dog
+ * dog_t - new name of the strct dog
  * @name: name parameter of the dog
  * @age: age parameter of the dog
  * @owner: name of owner of the dog
@@ -12,7 +12,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
