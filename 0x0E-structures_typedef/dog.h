11fb59bfffa211a20415aeedbc0048371ae60196
bowashon <baidoofelix05@gmail.com>
2023-06-20 00:31:46 +0000

    changes made

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - defines new type with elements
+ * @name: name parameter of the dog
+ * @age: age parameter of the dog
+ * @owner: name of owner of the dog
+ */
 struct dog
 {
 	char *name;
