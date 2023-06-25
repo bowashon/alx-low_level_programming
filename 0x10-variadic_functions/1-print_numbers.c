b6303056bd192e710e8695de2e9119397b3030ef
bowashon <baidoofelix05@gmail.com>
2023-06-25 17:05:28 +0000

    new changes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list user_input;
 	int num;
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	va_start(user_input, n);
 
 	for (i = 0; i < n; i++)
