d4a1f93de011bd03b50bd9a2b38e4c47557779cb
bowashon <baidoofelix05@gmail.com>
2023-07-01 01:53:48 +0000

    read data from node

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - function thats prints nodes
+ * @h: pointer parameter
+ * Return: return number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%u] %s\n", current->len, current->str);
+		}
+		count++;
+
+		current = current->next;
+	}
+
+	return (count);
+}
