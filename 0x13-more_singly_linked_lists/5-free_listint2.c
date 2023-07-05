3240b7919fc0852341f90ac92495717143263ac1
bowashon <baidoofelix05@gmail.com>
2023-07-05 20:47:30 +0000

    changes made

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a listint_t
+ * @head: pointer to the next element
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
