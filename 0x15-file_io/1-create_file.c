a4b46834987f4b28e419796fec0a39f4f74caa37
bowashon <baidoofelix05@gmail.com>
2023-07-14 04:44:08 +0000

    changes made

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		text_written = write(file, text_content, len);
 	}
