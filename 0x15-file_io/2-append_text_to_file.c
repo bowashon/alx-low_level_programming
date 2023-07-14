d7f2c063e87d6249adea27272a05203abd39f84d
bowashon <baidoofelix05@gmail.com>
2023-07-14 05:30:08 +0000

    append text to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - function that appends texts to a file
+ * @filename: represent the name of the file
+ * @text_content: content to write to the file
+ * Return: return 1 if success and -1 if error
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	size_t len = strlen(text_content), text_written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		text_written = write(file, text_content, len);
+	}
+	if (text_written != len)
+	{
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
