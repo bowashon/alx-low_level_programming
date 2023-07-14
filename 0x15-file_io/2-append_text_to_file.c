46ae9813fe1441472690a6a0e08ba8ffe55a8f34
bowashon <baidoofelix05@gmail.com>
2023-07-14 05:42:07 +0000

    changes made

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
-	size_t len = strlen(text_content), text_written = 0;
+	ssize_t len = strlen(text_content), text_written = 0;
 
 	if (filename == NULL)
 	{
@@ -21,11 +21,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		text_written = write(file, text_content, len);
 	}
+
+	close(file);
+
 	if (text_written != len)
 	{
 		return (-1);
 	}
 
-	close(file);
 	return (1);
 }
