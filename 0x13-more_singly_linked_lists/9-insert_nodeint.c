0355e5b6143e1736590804c0e875affc11b02708
bowashon <baidoofelix05@gmail.com>
2023-08-26 15:42:35 +0000

    insert new node at index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_nodeint_at_index - function that inserts a new node
+ * at index
+ * @head: points to the head
+ * @idx: index of insertion
+ * @n: value or node data
+ * Return: the address of the new node or NULL is otherwise
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *current;
+	unsigned int i;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+
+	current = *head;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
+	for (i = 0; i < idx - 1; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		return (NULL);
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
+}
