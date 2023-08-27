8c6a2f41ce73398249593d1b0f58aeefcf83a9ec
bowashon <baidoofelix05@gmail.com>
2023-08-27 07:10:12 +0000

    free dlistint and get node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_dlistint - function that frees a node in doubly 
+ * linked list
+ * @head: points to the head
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next, *current;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * get_dnodeint_at_index - function that returns node at index
+ * @head: points to the head
+ * @index: index at which the node is to be retrieved
+ * Return: return the node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i;
+
+	current = head;
+
+	if (index == 0)
+	{
+		return (head);
+	}
+
+	for (i = 0; i < index && current != NULL; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		perror("node does not exist\n");
+		return (NULL);
+	}
+
+	return (current);
+}
