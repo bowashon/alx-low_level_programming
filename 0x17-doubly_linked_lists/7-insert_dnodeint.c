2217414ea42004a0663118a8801d64f9efe8582d
bowashon <baidoofelix05@gmail.com>
2023-08-27 10:35:07 +0000

    changes made

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,12 +17,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
-		dprintf(2, "Error: Failed to malloc\n");
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
 	if (idx == 0)
 	{
 		new_node->next = *h;
@@ -43,7 +40,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new_node->next = current->next;
 	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
 	current->next = new_node;
-	current->next->prev = new_node;
 	return (new_node);
 }
