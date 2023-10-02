b4db82a6befb16568e406b5bd77546b00ca22de9
bowashon <baidoofelix05@gmail.com>
2023-10-02 11:24:09 +0000

    get value function added

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_get - function that returns the value associated with the key
+ * @ht: table item
+ * @key: points to strings that represent the key
+ * Return: return the value or NULL if not any
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	long unsigned int index;
+	hash_node_t *current;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
