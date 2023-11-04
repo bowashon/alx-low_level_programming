8c9a6a7bf2d5b5eb78f0d5bbcd560f9fa5b448d4
bowashon <baidoofelix05@gmail.com>
2023-11-04 11:51:11 +0000

    lookup function added

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: points to the hash table
+ * @key: points to the string the representing the key
+ * Return: returns the value associated or NUll if couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *current;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
