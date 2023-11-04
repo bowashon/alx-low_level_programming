68aaa774cc9471c9966cb4b2bca64a4aeb30f097
bowashon <baidoofelix05@gmail.com>
2023-11-04 14:12:33 +0000

    changes made

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	current = ht->array[index];
